5b70dbdbc98194d7ac471314b371f06c1f54e2bf
nashehannafii <nashehannafii@gmail.com>
2026-03-01 12:48:10 +0700

    u

--- views/system/_form.php
+++ views/system/_form.php
@@ -22,7 +22,7 @@ use yii\widgets\ActiveForm;
             <?= $form->field($model, 'id')->hiddenInput(['value' => $uuid ?? $model->id])->label(false) ?>
 
             <div class="row g-3">
-                <div class="col-md-6">
+                <div class="col-md-3">
                     <div class="form-floating">
                         <?= $form->field($model, 'system_code')->textInput([
                             'maxlength' => true,
@@ -32,7 +32,7 @@ use yii\widgets\ActiveForm;
                     </div>
                 </div>
 
-                <div class="col-md-6">
+                <div class="col-md-9">
                     <div class="form-floating">
                         <?= $form->field($model, 'system_name')->textInput([
                             'maxlength' => true,
@@ -42,14 +42,14 @@ use yii\widgets\ActiveForm;
                     </div>
                 </div>
 
-                <div class="col-md-6">
+                <div class="col-md-9">
                     <?= $form->field($model, 'system_type')->dropDownList([
                         'mysql' => 'MySQL',
                         'postgres' => 'PostgreSQL'
                     ], ['prompt' => 'Select Type', 'class' => 'form-select'])->label('System Type') ?>
                 </div>
 
-                <div class="col-md-6">
+                <div class="col-md-3">
                     <?= $form->field($model, 'hostname')->textInput([
                         'maxlength' => true,
                         'class' => 'form-control',
@@ -57,7 +57,7 @@ use yii\widgets\ActiveForm;
                     ])->label('Hostname') ?>
                 </div>
 
-                <div class="col-md-6">
+                <div class="col-md-9">
                     <?= $form->field($model, 'password')->passwordInput([
                         'maxlength' => true,
                         'class' => 'form-control',
