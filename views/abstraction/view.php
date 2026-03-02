426dc42341b5ec84d8b10385ced78b6c850b0b79
nashehannafii <nashehannafii@gmail.com>
2026-03-02 23:15:48 +0700

    u

--- views/abstraction/view.php
+++ views/abstraction/view.php
@@ -89,51 +89,31 @@ $this->params['breadcrumbs'][] = $this->title;
                     <div class="modal-dialog">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalgridLabel">Grid Modals</h5>
+                                <h5 class="modal-title" id="exampleModalgridLabel">Abstraction Column <?= $model->table_name ?></h5>
                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div class="modal-body">
                                 <form action="javascript:void(0);">
                                     <div class="row g-3">
+                                        <input type="hidden" class="form-control" id="table-id" value="">
+                                        <input type="hidden" class="form-control" id="table-name" value="<?= Html::encode($model->table_name) ?>">
+
                                         <div class="col-xxl-6">
                                             <div>
-                                                <label for="firstName" class="form-label">First Name</label>
+                                                <label for="firstName" class="form-label">Column Type</label>
                                                 <input type="text" class="form-control" id="firstName" placeholder="Enter firstname">
                                             </div>
                                         </div><!--end col-->
                                         <div class="col-xxl-6">
                                             <div>
-                                                <label for="lastName" class="form-label">Last Name</label>
+                                                <label for="lastName" class="form-label">Column Warehouse</label>
                                                 <input type="text" class="form-control" id="lastName" placeholder="Enter lastname">
                                             </div>
                                         </div><!--end col-->
-                                        <div class="col-lg-12">
-                                            <label for="genderInput" class="form-label">Gender</label>
-                                            <div>
-                                                <div class="form-check form-check-inline">
-                                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
-                                                    <label class="form-check-label" for="inlineRadio1">Male</label>
-                                                </div>
-                                                <div class="form-check form-check-inline">
-                                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
-                                                    <label class="form-check-label" for="inlineRadio2">Female</label>
-                                                </div>
-                                                <div class="form-check form-check-inline">
-                                                    <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3">
-                                                    <label class="form-check-label" for="inlineRadio3">Others</label>
-                                                </div>
-                                            </div>
-                                        </div><!--end col-->
                                         <div class="col-xxl-6">
                                             <div>
-                                                <label for="emailInput" class="form-label">Email</label>
-                                                <input type="email" class="form-control" id="emailInput" placeholder="Enter your email">
-                                            </div>
-                                        </div><!--end col-->
-                                        <div class="col-xxl-6">
-                                            <div>
-                                                <label for="passwordInput" class="form-label">Password</label>
-                                                <input type="password" class="form-control" id="passwordInput" value="451326546">
+                                                <label for="lastName" class="form-label">Description</label>
+                                                <input type="text" class="form-control" id="description" placeholder="Enter description">
                                             </div>
                                         </div><!--end col-->
                                         <div class="col-lg-12">
