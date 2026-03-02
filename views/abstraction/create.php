bb58c844da72ea9aa1aaa063854d51e773830394
nashehannafii <nashehannafii@gmail.com>
2026-03-02 23:27:27 +0700

    u

--- views/abstraction/create.php
+++ views/abstraction/create.php
@@ -15,7 +15,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?= $this->render('_form', [
         'model' => $model,
-        'uuid' => $uuidd,
+        'uuid' => $uuid,
     ]) ?>
 
 </div>
