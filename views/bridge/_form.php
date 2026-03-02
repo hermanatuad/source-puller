18141f616b0466d2e924be3f5397142b6339f295
nashehannafii <nashehannafii@gmail.com>
2026-03-02 22:55:13 +0700

    u

--- views/bridge/_form.php
+++ views/bridge/_form.php
@@ -22,9 +22,7 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'bridge_target')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'created_at')->textInput() ?>
-
-    <?= $form->field($model, 'updated_at')->textInput() ?>
+    <br>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
