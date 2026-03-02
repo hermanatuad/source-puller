325efa54f9151888f374c1758879b2d36bad63fc
nashehannafii <nashehannafii@gmail.com>
2026-03-02 15:47:08 +0700

    u

--- views/entity/view.php
+++ views/entity/view.php
@@ -6,37 +6,64 @@ use yii\widgets\DetailView;
 /** @var yii\web\View $this */
 /** @var app\models\Entity $model */
 
-$this->title = $model->id;
+$this->title = $model->entity_id;
 $this->params['breadcrumbs'][] = ['label' => 'Entities', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
-<div class="entity-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card">
+            <div class="card-header align-items-center d-flex">
+                <h4 class="card-title mb-0 flex-grow-1">
+                    <i class="ri-server-line me-2"></i>Entity Details
+                </h4>
+            </div>
 
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="text-center border-end">
+                            <div class="avatar-xl mx-auto mb-3">
+                                <div class="avatar-title bg-primary-subtle text-primary rounded-circle fs-1">
+                                    <?= Html::encode(strtoupper(substr($model->entity_id, 0, 2))) ?>
+                                </div>
+                            </div>
+                            <h5 class="mb-1"><?= Html::encode($model->id) ?></h5>
+                            <p class="text-muted mb-2"><?= Html::encode($model->entity_id) ?></p>
+                            <span class="badge bg-info-subtle text-info mb-3">
+                                <?= Html::encode($model->is_alive) ?>
+                            </span>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <table class="table table-borderless mb-0">
+                            <tbody>
+                                <tr>
+                                    <th scope="row">
+                                        <i class="ri-code-s-slash-line me-2 text-muted"></i>Entity ID
+                                    </th>
+                                    <td><?= Html::encode($model->entity_id) ?></td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        <i class="ri-code-s-slash-line me-2 text-muted"></i>Life Status
+                                    </th>
+                                    <td><?= Html::encode($model->is_alive) ?></td>
+                                </tr>
+                                <tr>
+                                    <th scope="row">
+                                        <i class="ri-code-s-slash-line me-2 text-muted"></i>Last Update
+                                    </th>
+                                    <td><?= Html::encode($model->updated_at) ?></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-    <?= DetailView::widget([
-        'model' => $model,
-        'attributes' => [
-            'id',
-            'entity_id',
-            'status',
-            'created_at_data',
-            'updated_at_data',
-            'created_at',
-            'updated_at',
-        ],
-    ]) ?>
-
+    </div>
 </div>
