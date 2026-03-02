a537bc1a98a82b6649f543e3e58435956897c6ce
nashehannafii <nashehannafii@gmail.com>
2026-03-02 23:16:52 +0700

    u

--- views/abstraction/view.php
+++ views/abstraction/view.php
@@ -89,7 +89,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     <div class="modal-dialog">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalgridLabel">Abstraction Column <?= $model->table_name ?></h5>
+                                <h5 class="modal-title" id="exampleModalgridLabel">Abstraction Column <?= $model->id ?></h5>
                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div class="modal-body">
