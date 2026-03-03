c9f1e268ab066e2fbce795ed37da8366f9e0697f
nashehannafii <nashehannafii@gmail.com>
2026-03-03 21:23:56 +0700

    u

--- views/bridge/_form.php
+++ views/bridge/_form.php
@@ -25,6 +25,109 @@ use app\models\System;
 
             <div class="row g-3">
 
+                <div>
+                    <h5 class="fs-14 mb-3">Single select input Example</h5>
+
+                    <div class="row">
+                        <div class="col-lg-4 col-md-6">
+                            <div class="mb-3">
+                                <label for="choices-single-default" class="form-label text-muted">Default</label>
+                                <p class="text-muted">Set <code>data-choices</code> attribute to set a default single select.</p>
+                                <select class="form-control" data-choices name="choices-single-default" id="choices-single-default">
+                                    <option value="">This is a placeholder</option>
+                                    <option value="Choice 1">Choice 1</option>
+                                    <option value="Choice 2">Choice 2</option>
+                                    <option value="Choice 3">Choice 3</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6">
+                            <div class="mb-3">
+                                <label for="choices-single-groups" class="form-label text-muted">Option Groups</label>
+                                <p class="text-muted">Set <code>data-choices data-choices-groups</code> attribute to set option group</p>
+                                <select class="form-control" id="choices-single-groups" data-choices data-choices-groups data-placeholder="Select City" name="choices-single-groups">
+                                    <option value="">Choose a city</option>
+                                    <optgroup label="UK">
+                                        <option value="London">London</option>
+                                        <option value="Manchester">Manchester</option>
+                                        <option value="Liverpool">Liverpool</option>
+                                    </optgroup>
+                                    <optgroup label="FR">
+                                        <option value="Paris">Paris</option>
+                                        <option value="Lyon">Lyon</option>
+                                        <option value="Marseille">Marseille</option>
+                                    </optgroup>
+                                    <optgroup label="DE" disabled>
+                                        <option value="Hamburg">Hamburg</option>
+                                        <option value="Munich">Munich</option>
+                                        <option value="Berlin">Berlin</option>
+                                    </optgroup>
+                                    <optgroup label="US">
+                                        <option value="New York">New York</option>
+                                        <option value="Washington" disabled>Washington</option>
+                                        <option value="Michigan">Michigan</option>
+                                    </optgroup>
+                                    <optgroup label="SP">
+                                        <option value="Madrid">Madrid</option>
+                                        <option value="Barcelona">Barcelona</option>
+                                        <option value="Malaga">Malaga</option>
+                                    </optgroup>
+                                    <optgroup label="CA">
+                                        <option value="Montreal">Montreal</option>
+                                        <option value="Toronto">Toronto</option>
+                                        <option value="Vancouver">Vancouver</option>
+                                    </optgroup>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6">
+                            <div class="mb-3">
+                                <label for="choices-single-no-search" class="form-label text-muted">Options added via config with no search</label>
+                                <p class="text-muted">Set <code>data-choices data-choices-search-false data-choices-removeItem</code></p>
+                                <select class="form-control" id="choices-single-no-search" name="choices-single-no-search" data-choices data-choices-search-false data-choices-removeItem>
+                                    <option value="Zero">Zero</option>
+                                    <option value="One">One</option>
+                                    <option value="Two">Two</option>
+                                    <option value="Three">Three</option>
+                                    <option value="Four">Four</option>
+                                    <option value="Five">Five</option>
+                                    <option value="Six">Six</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div class="col-lg-4 col-md-6">
+                            <div class="mb-3">
+                                <label for="choices-single-no-sorting" class="form-label text-muted">Options added via config with no sorting</label>
+                                <p class="text-muted">Set <code>data-choices data-choices-sorting-false</code> attribute.</p>
+                                <select class="form-control" id="choices-single-no-sorting" name="choices-single-no-sorting" data-choices data-choices-sorting-false>
+                                    <option value="Madrid">Madrid</option>
+                                    <option value="Toronto">Toronto</option>
+                                    <option value="Vancouver">Vancouver</option>
+                                    <option value="London">London</option>
+                                    <option value="Manchester">Manchester</option>
+                                    <option value="Liverpool">Liverpool</option>
+                                    <option value="Paris">Paris</option>
+                                    <option value="Malaga">Malaga</option>
+                                    <option value="Washington" disabled>Washington</option>
+                                    <option value="Lyon">Lyon</option>
+                                    <option value="Marseille">Marseille</option>
+                                    <option value="Hamburg">Hamburg</option>
+                                    <option value="Munich">Munich</option>
+                                    <option value="Barcelona">Barcelona</option>
+                                    <option value="Berlin">Berlin</option>
+                                    <option value="Montreal">Montreal</option>
+                                    <option value="New York">New York</option>
+                                    <option value="Michigan">Michigan</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- end row -->
+                </div>
+                
                 <div class="col-md-6">
                     <?= $form->field($model, 'bridge_target')->dropDownList($abstraction, [
                         'prompt' => 'Select abstraction',
