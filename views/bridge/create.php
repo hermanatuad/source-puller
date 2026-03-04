03541146da83fa8a8a0182778202d238b3aaaf5b
nashehannafii <nashehannafii@gmail.com>
2026-03-04 21:55:17 +0700

    u

--- views/bridge/create.php
+++ views/bridge/create.php
@@ -5,8 +5,8 @@ use yii\helpers\Html;
 /** @var yii\web\View $this */
 /** @var app\models\Bridge $model */
 
-$this->title = 'Create Bridge';
-$this->params['breadcrumbs'][] = ['label' => 'Bridges', 'url' => ['index']];
+$this->title = 'Create Extraction Pipeline';
+$this->params['breadcrumbs'][] = ['label' => 'Extraction Pipelines', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="bridge-create">
--- views/bridge/update.php
+++ views/bridge/update.php
@@ -5,8 +5,8 @@ use yii\helpers\Html;
 /** @var yii\web\View $this */
 /** @var app\models\Bridge $model */
 
-$this->title = 'Update Bridge: ' . $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Bridges', 'url' => ['index']];
+$this->title = 'Update Extraction Pipeline: ' . $model->id;
+$this->params['breadcrumbs'][] = ['label' => 'Extraction Pipelines', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
