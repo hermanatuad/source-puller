9aba3250681c54d90f30fbdf473742c587df515c
nashehannafii <nashehannafii@gmail.com>
2026-03-09 14:04:42 +0700

    u

--- web/themes/velzon/layouts/setting.php
+++ web/themes/velzon/layouts/setting.php
@@ -1,1 +1,11 @@
-<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none" data-bs-theme="light" data-body-image="img-1" data-preloader="enable">
+<html
+    lang="en"
+    data-layout="vertical"
+    data-topbar="light"
+    data-sidebar="dark"
+    data-sidebar-size="sm-hover"
+    data-sidebar-image="none"
+    data-bs-theme="light"
+    data-body-image="img-1"
+    data-preloader="enable"
+    data-layout-position="fixed">
