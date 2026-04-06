46b153a5e23cd353249f3050f9e2a8d992ad6ace
nashehannafii <nashehannafii@gmail.com>
2026-04-06 17:16:13 +0700

    update

--- views/system/_form.php
+++ views/system/_form.php
@@ -60,7 +60,6 @@ use yii\widgets\ActiveForm;
                         'prompt' => 'Select Type',
                         'class' => 'form-select',
                         'options' => [
-                            'sql-server' => ['disabled' => true],
                             'postgres' => ['disabled' => true],
                         ],
                     ])->label('System Type') ?>
