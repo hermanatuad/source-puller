70f03f8d9313f00d9dad7dbdb526be31821ed15f
nashehannafii <nashehannafii@gmail.com>
2026-04-14 10:31:14 +0700

    xml

--- config/web.php
+++ config/web.php
@@ -73,8 +73,8 @@ $config = [
                 'rbac/<action:\w+>' => 'rbac/<action>',
                 'students' => 'students/index',
                 '<controller:\w+>/<id:\d+>' => '<controller>/view',
-                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
-                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:[\w-]+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:[\w-]+>' => '<controller>/<action>',
             ],
         ],
         'view' => [
